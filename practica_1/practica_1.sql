646229051165c8abaeefc562829ea0588ec56ca0
plablo09 <plablo09@gmail.com>
2014-05-30 11:08:13 -0500

    Nuevos ejercicios en la práctica 1 y reorganización de los archivos

--- practica_1/practica_1.sql
+++ practica_1/practica_1.sql
@@ -13,24 +13,33 @@ st_intersects(limite_metropolitano.geom,manzanas.geom);
 --Nota como lo que hicimos fue en realidad un inner join pero como condición utilizamos una relación espacial: st_intersects
 
 --creamos un índice espacial sobre la geometría
-create index calles_zmvm_gix on calles_zmvm using GIST(geom);
+create index manzanas_zmvm_gix on manzanas_zmvm using GIST(geom);
 
 --vemos si los gid son únicos
-select count(*) from calles_zmvm  group by gid order by count(*) desc;
+select count(*) from manzanas_zmvm  group by gid order by count(*) desc;
 
 --como la tabla tiene unos gid's repetidos, alteramos la columna para que sean únicos
-CREATE SEQUENCE "calles_zmvm_gid_seq";
-update calles_zmvm set gid = nextval('"calles_zmvm_gid_seq"');
+CREATE SEQUENCE "manzanas_zmvm_gid_seq";
+update manzanas_zmvm set gid = nextval('"manzanas_zmvm_gid_seq"');
 
 --Creamos los constraints necesarios y agregamos un PK
-ALTER TABLE calles_zmvm ALTER COLUMN "gid" SET NOT NULL;
-ALTER TABLE calles_zmvm ADD UNIQUE ("gid");
-ALTER TABLE calles_zmvm ADD PRIMARY KEY ("gid");
+ALTER TABLE manzanas_zmvm ALTER COLUMN "gid" SET NOT NULL;
+ALTER TABLE manzanas_zmvm ADD UNIQUE ("gid");
+ALTER TABLE manzanas_zmvm ADD PRIMARY KEY ("gid");
 
 
 --Ahora podemos empezar a hacer algunas preguntas interesantes, por ejemplo:
 --¿cuantas manzanas quedan a 500 metros de cada estación del metro?
-(with buf as (select st_buffer(estaciones_metro.geom,500.0) as geom , estaciones.nombre as estacion from estaciones)
+select foo.* from
+(with buf as (select st_buffer(estaciones_metro.geom,500.0) as geom , estaciones_metro.nombreesta as estacion from estaciones_metro)
 select count(manzanas_zmvm.gid), buf.estacion from manzanas_zmvm join buf on 
+st_intersects(buf.geom,manzanas_zmvm.geom)
+group by buf.estacion) as foo;
+
+--Tambien podemos preguntar ¿Cuánta gente vive a 500 m de una estación del metro?
+--Nota: La columna pob1 contiene la población de cada manzana
+select foo.* from
+(with buf as (select st_buffer(estaciones_metro.geom,500.0) as geom , estaciones_metro.nombreesta as estacion from estaciones_metro)
+select sum(manzanas_zmvm.pob1), buf.estacion from manzanas_zmvm join buf on 
 st_intersects(buf.geom,manzanas_zmvm.geom)
 group by buf.estacion) as foo;

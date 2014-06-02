3308f6ce28d5b5ab45ae587106b1f2c1348c399c
plablo09 <plablo09@gmail.com>
2014-06-02 13:12:13 -0500

    Lista la primera parte de la práctica 2

---
+++ practica_2/practica_2.md
@@ -0,0 +1,38 @@
+Instructivo para la segunda práctica de SIG Usando Postgres/PostGis. 
+Primitivos espaciales; armando y desarmando; relaciones espaciales.
+=============
+
+En esta práctica trataremos dos cuestiones fundamentales sobre la representación en base de datos de información geoespacial:
+1. Los primitivos espaciales (puntos, lineas y polígonos) y sus tipos agregados
+2. Las relaciones espaciales
+
+Para esta práctica utilizaremos las capas de datos que puedes encontrar en la carpeta 'practica_2/data/' de este repositorio. 
+Recuerda que antes de pasar al archivo sql, debes subir todos los _shapes_ de dicha carpeta a tablas en Postgis, para efectos del resto del instructivo, asumiremos que subiste las capas en una base de datos llamada practicas_sig y en el esquema practica_2, como se ve en el siguiente esquema:
+
+*practicas_sig
+	*practica_1
+		*tablas...
+	*practica_2
+		*tablas...
+
+Ejercicios		
+=============
+
+1. En el primer ejercicio vamos construir un polígono a partir de una serie de puntos (tomados con un GPS). 
+
+Una vez que tengas tus tablas en una base de datos, puedes proseguir al archivo practica_2.sql
+
+
+
+Referencia
+=============
+
+Para conocer más sobre el estándar de objetos espaciales (Simple Feature Access) puedes consultar el estándar (PDF) aquí:
+[OpenGIS Implementation Specification for Geographic information - Simple feature access - Part 1: Common architecture](http://portal.opengeospatial.org/files/?artifact_id=25355)
+ En dicho archivo encontrarás toda la información sobre las definiciones de los primitivos espaciales y sus representaciones.
+ 
+Para una introducción a los objetos geométricos, puedes seguir el tutorial de Boundless:
+[Geometrías en PostGis](http://workshops.boundlessgeo.com/postgis-intro/geometries.html)
+
+En general, conviene tener a la mano el manual de PostGis:
+[PostGis Manual](http://postgis.net/docs/manual-2.0/)
---
+++ practica_2/practica_2.sql
@@ -0,0 +1,81 @@
+--El primer paso es crear una linea a partir de los puntos de la tabla waypoints. La linea que creemos dependerá del orden en el que juntemos los puntos,
+--entonces, ordenémoslos por id:
+--La cláusula GROUP BY track_id, agrupa los puntos de acuerdo a un identificador de recorrido, 
+--en el caso de que tuvieramos más de uno. (Guarda nota de esto porque será importante en los ejercicios)
+
+CREATE TABLE practica_2.gps_tracks AS
+SELECT
+ST_MakeLine(geom) AS geom,
+track_id
+FROM (
+SELECT * FROM practica_2.waypoints
+ORDER BY id
+) AS ordered_points
+GROUP BY track_id;
+
+--¿Que tipo de geometría generamos al unir los puntos? Veamos:
+SELECT ST_asText(geom) from practica_2.gps_tracks;
+
+-- Si visualizas la capa en QGis, notarás que la linea que construimos contiene una auto-intersección y que no está cerrada
+-- (el primero y el último punto no coinciden)
+-- Para resolver este problema, vamos a generar un nodo en la auto-intersección:
+--[Pregunta # 1: ¿Qué hace el operador ST_UnaryUnion() y, más en general, ¿Qué hace una unión en Postgis?]
+
+UPDATE practica_2.gps_tracks
+SET geom = ST_UnaryUnion(geom)
+
+-- Para ver el resultado de la operación anterior, vamos a desbaratar la linea que creamos en sus componentes:
+
+SELECT  ST_asText(ST_GeometryN(geom,generate_series(1,ST_NumGeometries(geom)))) AS lines
+FROM practica_2.gps_tracks
+
+--Esto nos regresa un conjunto de objetos tipo Linestring (véanlo). 
+--Aquí utilizamos dos funciones nuevas; ST_GeometryN() y ST_NumGeometries(), consulta la documentación de Postgis para
+--entender su significado.
+
+--Ahora que hemos separado el Multilinestring en sus componentes, vamos a desarmar éstas líneas en sus 
+--componentes básicos:
+
+SELECT ST_AsText(
+   ST_PointN(
+	  lines,
+	  generate_series(1, ST_NPoints(lines))
+   ))
+FROM (
+	SELECT 
+		ST_GeometryN(geom,
+	generate_series(1,ST_NumGeometries(geom))) AS lines
+	FROM practica_2.gps_tracks
+) AS foo;
+
+--Nota que hicimos las dos consultas anidadas, es decir, primero hacemos la consulta anterior y luego la envolvemos 
+--con la consulta que nos regresa los puntos.
+--Ahora, para poder visualizar el resultado en QGis, necesitamos agregar un id a los puntos y ponerlos en una tabla:
+
+CREATE TABLE practica_2.waypoints_nuevos AS
+SELECT 
+   ST_PointN(
+	  lines,
+	  generate_series(1, ST_NPoints(lines))
+   ) as geom
+FROM (
+	SELECT 
+		ST_GeometryN(geom,
+	generate_series(1,ST_NumGeometries(geom))) AS lines
+	FROM practica_2.gps_tracks
+) AS foo;
+
+alter table practica_2.waypoints_nuevos add column gid serial;
+
+--Como podemos ver, creamos un punto el la auto-intersección. Ahora regresemos a la tabla gps_tracks y creemos un polígono:
+CREATE TABLE gps_lakes AS
+SELECT
+ST_BuildArea(the_geom) AS lake,
+track_id
+FROM gps_tracks;
+
+--Ahora pueden visualizar el lago en QGis. PREGUNTA #2: ¿Qué hubiera pasado si no ponemos un nodo en la auto-intersección?
+--Hint: ver en la documentación--> ¿Qué es un polígono?
+
+--EJERCICIO # 1 
+

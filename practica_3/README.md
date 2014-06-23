a3c944896fc070f03900f58980a9f8e830418a8f
plablo09 <plablo09@gmail.com>
2014-06-23 13:40:16 -0500

    Primer ejercicio de la práctica 3

--- practica_3/README.md
+++ practica_3/README.md
@@ -1,4 +1,32 @@
 Práctica 3 Análisis de redes con pg_routing
 =========
 
-En el primer ejercicio vamos a utilizar la extensión pgrouting para crear una topología de redes a la tabla de calles_zmvm en el esquema practica_3
+En el primer ejercicio vamos a utilizar la extensión pgrouting para crear una
+topología de redes a la tabla de calles_zmvm en el esquema practica_3
+
+En primer lugar, vamos a copiar la tabla calles_zmvm  al esquema practica_3:
+
+````sql
+CREATE TABLE practica_3.calles_zmvm AS
+TABLE practica_1.calles_zmvm;
+````
+
+Para hacer análisis de redes en PsotGis, necesitamos crear la extensión pgrouting
+en la base de datos:
+
+````sql
+CREATE EXTENSION pgrouting;
+````
+Nota: pgrouting es una extensión de PostGis para hacer análisis de redes, la
+documentación completa la puedes encontrar aqui:
+ [Docs de pgrouting](http://docs.pgrouting.org/2.0/en/doc/index.html)
+
+Ahora, como recordarán del ejercicio en Arc, necesitamos crear columnas en la tabla que nos indiquen
+origen y destino para crear una topología de red:
+
+````sql
+ALTER TABLE practica_3.calles_zmvm ADD COLUMN source integer;
+ALTER TABLE practica_3.calles_zmvm ADD COLUMN target integer;
+````
+
+Ahora estamos listos para proceder a los ejercicios, abre el archivo practica_3.sql
--- practica_3/ejercicio_1.sql
+++ practica_3/ejercicio_1.sql
@@ -1,7 +1,37 @@
+
+--Esta primera sección contiene las consultas del README, si ya las
+--ejecutaste no es necesario que las vuelvas a correr.
+
+--Crear la extensión pgrouting
 create extension pgrouting;
 
-alter table practica_3.calles_zmvm add column source integer;
+--Crear las columnas source y target
+ALTER TABLE practica_3.calles_zmvm ADD COLUMN source integer;
+ALTER TABLE practica_3.calles_zmvm ADD COLUMN target integer;
 
-alter table practica_3.calles_zmvm add column target integer;
+--Ahora agregamos columnas para guardar los puntos de inicio y fin de
+--cada segmento. Esto sirve principalmente para establecer los sentidos
+--de las calles.
+alter table practica_3.calles_zmvm
+ADD COLUMN x1 double precision,
+ADD COLUMN y1 double precision,
+ADD COLUMN x2 double precision,
+ADD COLUMN y2 double precision;
 
-select pgr_createTopology('practica_3.calles_zmvm', 0.000001,'geom','gid');
+UPDATE practica_3.calles_zmvm SET
+x1 = st_x(st_startpoint(geom)),
+y1 = st_y(st_startpoint(geom)),
+x2 = st_x(st_endpoint(geom)),
+y2 = st_y(st_endpoint(geom));
+--Ahora tenemos en las columnas xi, las coordenadas x de los inicios y finales
+--de los segmentos y en las columnas yi las coordenadas y.
+
+--Ahr
+
+--Ahora vamos a agregar una columna con la longitud de cada segmento:
+
+ALTER TABLE practica_3.calles_zmvm ADD COLUMN length numeric;
+UPDATE practica_3.calles_zmvm SET length = st_length(geom)
+
+--Crear la topología de redes
+SELECT pgr_createTopology('practica_3.calles_zmvm', 0.001,'geom','gid');
